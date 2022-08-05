ccf066005a14a36ad48ddd02312b6c1c3c5212f6
ph-dev-labs <nwosuchimezie95@gmail.com>
2022-08-05 23:21:58 +0200

    started imlementing redux toolkit logic

--- src/App.js
+++ src/App.js
@@ -1,32 +1,75 @@
 import React from "react";
-import { Button, Box, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
 import "./App.css";
 
 function App() {
   return (
     <Box
       sx={{
-        width: 300,
-        height: 300,
+        width: 600,
+        height: 600,
         backgroundColor: "black",
-        "&:hover": {
-          backgroundColor: "primary.main",
-          opacity: [0.9, 0.8, 0.7],
-        },
+        margin: "auto",
       }}
     >
-      <div className="search">
-        <TextField id="outlined-basic" size="small" variant="outlined" />
-        <Button variant="outlined">Search</Button>
+      <div className="search-field">
+        <TextField
+          id="outlined-basic"
+          size="small"
+          variant="outlined"
+          color="primary"
+          type="text"
+          placeholder="Search city"
+          sx={{
+            backgroundColor: "white",
+            marginTop: "25px",
+            marginLeft: "55px",
+            width: "350px",
+            borderRadius: "6px",
+          }}
+        />
+        <SearchIcon
+          color="primary"
+          fontSize="large"
+          sx={{
+            margin: "10px",
+            marginTop: "25px",
+          }}
+        />
       </div>
-      <Typography variant="h4" component="h1">
+      <Typography
+        variant="h4"
+        component="h1"
+        color="primary"
+        sx={{
+          padding: "10px",
+          marginLeft: "70px",
+        }}
+      >
         weather in Nigeria
       </Typography>
-      <Typography variant="h2" component="h1">
+      <Typography
+        variant="h4"
+        component="h2"
+        color="primary"
+        sx={{
+          padding: "10px",
+          marginLeft: "70px",
+        }}
+      >
         52"c
       </Typography>
-      <Typography variant="h7" component="p">
+      <Typography
+        variant="h7"
+        component="p"
+        color="primary"
+        sx={{
+          padding: "10px",
+          marginLeft: "70px",
+        }}
+      >
         part cloudy and rainy
       </Typography>
     </Box>
---
+++ src/redux/weatherApi.js
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/react";
+const headers = {
+  "X-RapidAPI-Key": "65df072934msh14af832281348a6p1c224djsn87f4899cc1e0",
+  "X-RapidAPI-Host": "open-weather13.p.rapidapi.com",
+};
+
+const baseUrl = 'https://open-weather13.p.rapidapi.com'
